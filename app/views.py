20a3ba797356c709fcd31c3ea82c685dc73874ed
toddkuehnl <todd.kuehnl@pixovr.com>
2020-06-21 10:45:35 -0400

    improved model quality

--- app/views.py
+++ app/views.py
@@ -6,8 +6,12 @@ import os
 
 from OCC.Extend.DataExchange import read_step_file
 from OCC.Core.Tesselator import ShapeTesselator
+from OCC.Extend.TopologyUtils import TopologyExplorer
+from OCC.Core.STEPControl import STEPControl_Reader
+from OCC.Core.IFSelect import IFSelect_RetDone, IFSelect_ItemsByEntity
 
-app.config["MODEL_UPLOADS"] = "/home/todd_kuehnl/atdapi/app/uploads"
+#app.config["MODEL_UPLOADS"] = "/home/todd_kuehnl/atdapi/app/uploads"
+app.config["MODEL_UPLOADS"] = "C:/Users/todd/atd/atdapi/app/uploads"
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["STEP"]
 
 def allowed_image(filename):
@@ -55,15 +59,23 @@ def upload_model():
             modelfile = os.path.join(app.config["MODEL_UPLOADS"], model.filename)
             model.save(modelfile)
             filename, file_extension = os.path.splitext(model.filename)
-            big_shp = read_step_file(modelfile)
-            tess = ShapeTesselator(big_shp)
-            tess.Compute(compute_edges=False, mesh_quality=1)
-            jsonfile = filename + ".json"
-            with open(os.path.join(app.config["MODEL_UPLOADS"], jsonfile), "w") as text_file:
-                json_shape = tess.ExportShapeToThreejsJSONString(filename)
-                json_shape = json_shape.replace("data\\", "data/")
-                json_shape = json_shape.replace("\\step_postprocessed\\", "/step_postprocessed/")
-                text_file.write(json_shape)
+            #big_shp = read_step_file(modelfile)
+            step_reader = STEPControl_Reader()
+            status = step_reader.ReadFile(modelfile)
+            if status == IFSelect_RetDone:
+                ok = step_reader.TransferRoots()
+                _nbs = step_reader.NbShapes()
+                big_shp = step_reader.Shape()
+                tess = ShapeTesselator(big_shp)
+                tess.Compute(compute_edges=False, mesh_quality=0.5)
+                jsonfile = filename + ".json"
+                with open(os.path.join(app.config["MODEL_UPLOADS"], jsonfile), "w") as text_file:
+                    json_shape = tess.ExportShapeToThreejsJSONString(filename)
+                    json_shape = json_shape.replace("data\\", "data/")
+                    json_shape = json_shape.replace("\\step_postprocessed\\", "/step_postprocessed/")
+                    text_file.write(json_shape)
+            else:
+                raise Exception("error could not read step file")
             return redirect(url_for('modelview', modelname = jsonfile))
 
     return render_template("public/upload.html")
